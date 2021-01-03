77f70f38d18e8446d95225d90e38e528fe627c9a
Yudai Tanaka <ytnk531@gmail.com>
2021-01-03 15:15:20 +0900

    Initial commit.

---
+++ lib/prompt.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "prompt/version"
+
+module Prompt
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/prompt_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Prompt do
+  it "has a version number" do
+    expect(Prompt::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
